bee5cd5c8e98fdc742d3217edd30ea70b6ffdc6c
John-Joe <johnjoemly@gmail.com>
2025-07-24 20:51:44 +0100

    First commit a simple script opens blank window full screen

---
+++ .venv/Game scripts/Timbercraft_main.py
@@ -0,0 +1,15 @@
+import arcade
+
+class TimbercraftWindow(arcade.Window):
+    def __init__(self):
+        screen_width, screen_height = arcade.get_display_size()
+        super().__init__(screen_width, screen_height, title="Timbercraft", fullscreen=True)
+
+import arcade
+
+def main():
+    window = TimbercraftWindow()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
